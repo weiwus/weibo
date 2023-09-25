9ac145afc41cdb212d261906e3d386baee66beca
Wei JiangChao <weiwus@qq.com>
2023-09-25 05:19:26 +0000

    社交统计信息

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,6 +59,16 @@ class User extends Authenticatable
                     ->orderBy('created_at', 'desc');
     }
 
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
     public function follow($user_ids)
     {
         if ( ! is_array($user_ids)) {
